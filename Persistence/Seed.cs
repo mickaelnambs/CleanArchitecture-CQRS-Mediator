957509260a0450043abfa18525c246a38dac1b29
RANDRIANANTENAINA Nambinintsoa Mickaël <mickael.nambinintsoa07081999@gmail.com>
2023-10-18 20:57:41 +0400

    Seed users and refactor seed class

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,27 +1,50 @@
 using System.Text.Json;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
+        {
+            await SeedTodos(context);
+            await SeedUsers(context, userManager);
+        }
+
+        private static async Task SeedTodos(DataContext context)
         {
             if (await context.Todos.AnyAsync()) return;
 
-            var todosData = await File.ReadAllTextAsync("../Persistence/todos.json");
+            var todos = await DeserializeJsonFile<List<Todo>>("../Persistence/todos.json");
 
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            context.Todos.AddRange(todos);
 
-            var todos = JsonSerializer.Deserialize<List<Todo>>(todosData, options);
+            await context.SaveChangesAsync();
+        }
 
-            foreach (var todo in todos)
+        private static async Task SeedUsers(DataContext context, UserManager<AppUser> userManager)
+        {
+            if (await userManager.Users.AnyAsync()) return;
+
+            var users = await DeserializeJsonFile<List<AppUser>>("../Persistence/users.json");
+
+            foreach (var user in users)
             {
-                context.Add(todo);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
 
             await context.SaveChangesAsync();
+        }
+
+        private static async Task<T> DeserializeJsonFile<T>(string filePath)
+        {
+            var jsonData = await File.ReadAllTextAsync(filePath);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            return JsonSerializer.Deserialize<T>(jsonData, options);
         }
     }
 }
